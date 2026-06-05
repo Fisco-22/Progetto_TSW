81d9ceaf1436b80a4175c798ede813eeaf475dc7
francescoUni <f.pagliuchi@studenti.unisa.it>
2026-06-05 16:09:33 +0200

    creati getter e setter nel file bean

--- src/main/java/Model/Utente_Bean.java
+++ src/main/java/Model/Utente_Bean.java
@@ -12,4 +12,68 @@ public class Utente_Bean implements Serializable{
     private String indirizzo;
     private String dataNascita;
     private boolean newsletter;
+    
+    public Utente_Bean(){}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public String getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(String dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public boolean isNewsletter() {
+		return newsletter;
+	}
+
+	public void setNewsletter(boolean newsletter) {
+		this.newsletter = newsletter;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+    
+    
 }
