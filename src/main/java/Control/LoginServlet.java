b8f78fcfcf556e16621686136890181c4dbf9fa3
francescoUni <f.pagliuchi@studenti.unisa.it>
2026-06-05 17:15:59 +0200

    modifiche nel LoginServlet

--- src/main/java/Control/LoginServlet.java
+++ src/main/java/Control/LoginServlet.java
@@ -1,33 +1,47 @@
 package Control;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
+import Model.*;
+import DAO.*;
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+ 
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	private UtenteDAO dao = new UtenteDAO();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		
+		String actionType = request.getParameter("actionType");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		HttpSession session = request.getSession();
+		
+		if("login".equals(actionType)) {
+			Utente_Bean uAutentificato= dao.checkLogin(email, password);
+			
+			if(uAutentificato != null) {
+				session.setAttribute("user", uAutentificato);
+				session.setAttribute("userEmail", uAutentificato.getEmail());
+				RequestDispatcher dispatcher = request.getRequestDispatcher("view/...");
+			}
+		}
 	}
 
 	/**
