409ba9c37cba863cfa38b1cfbad3772787b7f75e
francescoUni <f.pagliuchi@studenti.unisa.it>
2026-06-05 16:08:22 +0200

    create variabili nel javabean

--- src/main/java/Model/Utente_Bean.java
+++ src/main/java/Model/Utente_Bean.java
@@ -3,5 +3,13 @@ package Model;
 import java.io.Serializable;
 
 public class Utente_Bean implements Serializable{
+	private static final long serialVersionUID = 1L;
 
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private String indirizzo;
+    private String dataNascita;
+    private boolean newsletter;
 }
