13b70371790b8025393be5fb1821995ff58d7a95
francescoUni <f.pagliuchi@studenti.unisa.it>
2026-06-12 16:27:17 +0200

    aggiunte constanti error in validate.js

---
+++ WebContent/scripts/validate.js
@@ -0,0 +1,7 @@
+let count = 1;
+const nameOrLastnameErrorMessage = "This field should contain only letters";
+const emailErrorMessage = "The email field should be in the form username@domain.ext";
+const phoneErrorMessage = "The number field should be in the form ###-#######";
+const emptyFieldErrorMessage = "This field cannot be empty"
+
+
