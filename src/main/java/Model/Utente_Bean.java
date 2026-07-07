8ac422b4138d7e69fb12f63d20d919ef6c0d3f81
francescoUni <f.pagliuchi@studenti.unisa.it>
2026-07-08 00:32:55 +0200

    sistemato utente bean

--- src/main/java/Model/Utente_Bean.java
+++ src/main/java/Model/Utente_Bean.java
@@ -22,27 +22,59 @@ public class Utente_Bean implements Serializable {
 	public Utente_Bean() {}
 
 	// --- GETTER E SETTER ---
-	public String getEmail() { return email; }
-	public void setEmail(String email) { this.email = email; }
+	public String getEmail() {
+		return email; 
+	}
+	public void setEmail(String email) {
+		this.email = email; 
+	}
 
-	public String getNome() { return nome; }
-	public void setNome(String nome) { this.nome = nome; }
+	public String getNome() {
+		return nome; 
+	}
+	public void setNome(String nome) { 
+		this.nome = nome; 
+	}
 
-	public String getCognome() { return cognome; }
-	public void setCognome(String cognome) { this.cognome = cognome; }
+	public String getCognome() { 
+		return cognome; 
+	}
+	public void setCognome(String cognome) { 
+		this.cognome = cognome; 
+	}
 
-	public String getPassword() { return password; }
-	public void setPassword(String password) { this.password = password; }
+	public String getPassword() { 
+		return password; 
+	}
+	public void setPassword(String password) { 
+		this.password = password; 
+	}
 
-	public String getIndirizzo() { return indirizzo; }
-	public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo; }
+	public String getIndirizzo() { 
+		return indirizzo; 
+	}
+	public void setIndirizzo(String indirizzo) { 
+		this.indirizzo = indirizzo; 
+	}
 
-	public String getDataNascita() { return dataNascita; }
-	public void setDataNascita(String dataNascita) { this.dataNascita = dataNascita; }
+	public String getDataNascita() { 
+		return dataNascita; 
+	}
+	public void setDataNascita(String dataNascita) { 
+		this.dataNascita = dataNascita; 
+	}
 
-	public String getRuolo() { return ruolo; }
-	public void setRuolo(String ruolo) { this.ruolo = ruolo; }
+	public String getRuolo() { 
+		return ruolo; 
+	}
+	public void setRuolo(String ruolo) { 
+		this.ruolo = ruolo; 
+	}
 
-	public String[] getTelefoni() { return telefoni; }
-	public void setTelefoni(String[] telefoni) { this.telefoni = telefoni; }
+	public String[] getTelefoni() { 
+		return telefoni; 
+	}
+	public void setTelefoni(String[] telefoni) { 
+		this.telefoni = telefoni; 
+	}
 }
