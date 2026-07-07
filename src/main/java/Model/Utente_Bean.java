f2ed9ab3f95287abfdb2f8e86e80256dd36668a9
francescoUni <f.pagliuchi@studenti.unisa.it>
2026-07-07 22:32:46 +0200

    migliorato il database e file bean

--- src/main/java/Model/Utente_Bean.java
+++ src/main/java/Model/Utente_Bean.java
@@ -5,90 +5,44 @@ import java.io.Serializable;
 public class Utente_Bean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	
-	private int id; 
+	// Nessun "id" numerico! La vera chiave primaria per te è l'Email
+	private String email;
 	private String nome;
 	private String cognome;
-	private String email;
 	private String password;
 	private String indirizzo;
 	private String dataNascita;
+	
+	// Il ruolo che nel database è ENUM('utente', 'admin')
+	private String ruolo; 
+	
+	// Array per i numeri dinamici
 	private String[] telefoni; 
-	private boolean admin; 
     
 	public Utente_Bean() {}
 
 	// --- GETTER E SETTER ---
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
 
-	public int getId() {
-		return id;
-	}
+	public String getNome() { return nome; }
+	public void setNome(String nome) { this.nome = nome; }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+	public String getCognome() { return cognome; }
+	public void setCognome(String cognome) { this.cognome = cognome; }
 
-	public String getNome() {
-		return nome;
-	}
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+	public String getIndirizzo() { return indirizzo; }
+	public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo; }
 
-	public String getCognome() {
-		return cognome;
-	}
+	public String getDataNascita() { return dataNascita; }
+	public void setDataNascita(String dataNascita) { this.dataNascita = dataNascita; }
 
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
+	public String getRuolo() { return ruolo; }
+	public void setRuolo(String ruolo) { this.ruolo = ruolo; }
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getIndirizzo() {
-		return indirizzo;
-	}
-
-	public void setIndirizzo(String indirizzo) {
-		this.indirizzo = indirizzo;
-	}
-
-	public String getDataNascita() {
-		return dataNascita;
-	}
-
-	public void setDataNascita(String dataNascita) {
-		this.dataNascita = dataNascita;
-	}
-
-	public String[] getTelefoni() {
-		return telefoni;
-	}
-
-	public void setTelefoni(String[] telefoni) {
-		this.telefoni = telefoni;
-	}
-
-	public boolean isAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(boolean admin) {
-		this.admin = admin;
-	}
+	public String[] getTelefoni() { return telefoni; }
+	public void setTelefoni(String[] telefoni) { this.telefoni = telefoni; }
 }
