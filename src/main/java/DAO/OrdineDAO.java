e165db7c38cf0887d6f69ea0e7776a89e14added
francescoUni <f.pagliuchi@studenti.unisa.it>
2026-07-19 01:29:38 +0200

    modificato ordineDAO

--- src/main/java/DAO/OrdineDAO.java
+++ src/main/java/DAO/OrdineDAO.java
@@ -32,7 +32,7 @@ public int salvaOrdine(Ordine_Bean ordine) {
 	String insertDettaglio = "INSERT INTO DETTAGLIO_ORDINE (Codice_Ordine, Codice_Viaggio, Destinazione, Prezzo_Acquisto, Data_Partenza, Num_Posti) VALUES (?, ?, ?, ?, ?, ?)";
 	
 	Connection con = null;
-	try {
+	try { 
 		con = getConnection();
 		con.setAutoCommit(false);
 		
@@ -45,6 +45,126 @@ public int salvaOrdine(Ordine_Bean ordine) {
 			ps.setString(5, ordine.getUltime4Cifre());
 			ps.executeUpdate();
 			
+			try(ResultSet rs = ps.getGeneratedKeys()){
+				if(!rs.next()) {
+					con.rollback();
+					return -1;
+				}
+				codiceOrdine = rs.getInt();
+			}
+		}
+		
+		try(PrepareStatement ps = con.prepareStatement(insertDettaglio)){
+			for(DettaglioOrdine_Bean d:ordine.getDettagli()) {
+				ps.setInt(1, codiceOrdine);
+				if(d.getCodiceViaggio() != null) {
+					ps.setInt(2, d.getCodiceViaggio());
+				} else {
+					ps.setNull(2, java.sql.Types.INTEGER);
+				}
+				ps.setString(3, d.getDestinazione());
+				ps.setDouble(4, d.getPrezzoAcquisto());
+				ps.setString(5, d.getDataPartenza());
+				ps.setInt(6, d.getNumPosti());
+				ps.executeUpdate();
+			}
+		}
+		
+		con.commit();
+		return codiceOrdine;
+	} catch (SQLException e) {
+		e.printStackTrace();
+		if(con!= null) {
+			try {
+				con.rollback();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+			
+		}
+		return -1;
+	} finally {
+		if(con != null) {
+			try {
+				con.setAutoCommit(true);
+				con.close();
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+public List<Ordine_Bean> getOrdiniByUtente(String email){
+	List<Ordine_Bean> ordini = new ArrayList<>();
+	String query = "SELECT * FROM ORDINE WHERE Email_Utente = ? ORDER BY Data_Ordine DESC";
+	
+	try(Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(query)) {
+		ps.setString(1, email);
+		try (ResultSet rs = ps.executeQuery()){
+			while(rs.next()) {
+				Ordine_Bean o = mappaOrdine(rs);
+				caricaDettaglio(con, o);
+				ordini.add(o);
+			}
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return ordini;
+}
+
+public List<Ordine_Bean> getOrdiniByPeriodo(String dataInizio, String dataFine){
+	List<Ordine_Bean> ordini = new ArrayList<>();
+	String query ="SELECT * FROM ORDINE WHERE DATE(Data_Ordine) BETWEEN ? AND ? ORDER BY Data_Ordine DESC";
+	
+	try(Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(query)){
+		ps.setString(1, dataInizio);
+		ps.setString(2, dataFine);
+		try (ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
+				Ordine_Bean o = mappaOrdine(rs);
+				caricaDettagli(con, o);
+				ordini.add(o);
+			}
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return ordini;
+	}
+
+private Ordine_Bean mappaOrdine(ResultSet rs) throws SQLException {
+	Ordine_Bean o = new Ordine_Bean();
+	o.setCodiceOrdine(rs.getInt("Codice_Ordine"));
+	o.setEmailUtente(rs.getString("Email_Utente"));
+	o.setDataOrdine(rs.getString("Data_Ordine"));
+	o.setTotaleOrdine(rs.getDouble("Totale_Ordine"));
+	o.setIndirizzoSpedizione(rs.getString("Indirizzo_Spedizione"));
+	o.setMetodoPagamento(rs.getString("Metodo_Pagamento"));
+	o.setUltime4Cifre(rs.getString("Ultime4Cifre"));
+	o.setStato(rs.getString("Stato"));
+	return o;
+}
+
+private void caricaDettaglio(Connection con, Ordine_Bean ordine) throws SQLException{
+	String query = "SELECT * FROM DETTAGLIO_ORDINE WHERE Codice_Ordine = ?";
+	try (PreparedStatement ps = con.prepareStatement(query)) {
+		ps.setInt(1, ordine.getCodiceOrdine());
+		try (ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
+				DettaglioOrdine_Bean d = new DettaglioOrdine_Bean();
+				d.setIdDettaglio(rs.getInt("ID_Dettaglio"));
+				d.setCodiceOrdine(rs.getInt("Codice_Ordine"));
+				int cv = rs.getInt("Codice_Viaggio");
+				d.setCodiceViaggio(rs.wasNull() ? null : cv);
+				d.setDestinazione(rs.getString("Destinazione"));
+				d.setPrezzoAcquisto(rs.getDouble("Prezzo_Acquisto"));
+				d.setDataPartenza(rs.getString("Data_Partenza"));
+				d.setNumPosti(rs.getInt("Num_Posti"));
+				ordine.aggiungiDettaglio(d);
+			}
 		}
 	}
 }
